d683b78b75537f8fdc15e28922795b738d623d09
Afik <aphex@aphex.cx>
2013-08-30 20:25:51 -0700

    cleanup comments and unused functionality

--- main.py
+++ main.py
@@ -15,19 +15,6 @@ class Trie(object):
             cur_dict[_end] = _end
         self.root = root
 
-    def __contains__(self, item):
-        cur_dict = self.root
-        for letter in item:
-            if letter in cur_dict:
-                cur_dict = cur_dict[letter]
-            else:
-                return False
-        else:
-            if _end in cur_dict:
-                return True
-            else:
-                return False
-
     def __repr__(self):
         return pprint.pformat(self.root)
 
@@ -38,49 +25,32 @@ def triemethod(string, words):
     word_ranges = set()
     for start_pos in xrange(len(string)):
         cur_dict = trie.root
-        subpos = start_pos
         curlen = 1
+        #check this substring
         for pos, letter in enumerate(string[start_pos:]):
-            print letter
             if letter in cur_dict:
+                #delve in to the trie
                 cur_dict = cur_dict[letter]
                 if _end in cur_dict:
                     #valid word
                     word_ranges.add((start_pos + pos+1-curlen, start_pos + pos+1))
-                    print word_ranges
                 curlen += 1
-                    # curlen = 1
-                    # cur_dict = trie.root
-                    # subpos = pos
             else:
                 cur_dict = trie.root
                 curlen = 1
-                subpos = pos
                 if letter in cur_dict:
+                    #delve in to the trie
                     cur_dict = cur_dict[letter]
                     if _end in cur_dict:
                         #valid word
                         word_ranges.add((pos-curlen, pos+1))
-                        print word_ranges
                     curlen += 1
-                        # curlen = 1
-                        # subpos = pos
-                # else: ???
-                    #subpos = pos + 1
 
-    print word_ranges
     valid_words = []
     for word_range in word_ranges:
         valid_words.append(string[slice(*word_range)])
 
-    #find valid word combos
-    remaining_range = (0, len(string))
-    complete_combo = []
-    for word_range in word_ranges:
-        remaining_range = (remaining_range[0] - word_range[0], remaining_range[1] - word_range[1])
-        for other_range in word_ranges:
-            pass
-
     return valid_words
 
 print triemethod('hotelfate', ['hotel', 'elf', 'fate', 'ate', 'hot'])
+
