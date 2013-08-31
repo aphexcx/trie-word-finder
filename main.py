9c8e843613fdd5fb17cd1020fd851a6594496544
Afik <aphex@aphex.cx>
2013-08-30 20:43:02 -0700

    simplify the logic a little bit, add some documentation and cleanup variable names for readability

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pprint
 
+#ending 'sentinel' character
 _end = '_end_'
 
 
@@ -19,38 +20,50 @@ class Trie(object):
         return pprint.pformat(self.root)
 
 
-def triemethod(string, words):
+def find_words(string, words):
+    """Using a trie, find all valid substrings of a given string given an
+     iterable of valid words."""
     trie = Trie(*words)
     print trie
     word_ranges = set()
-    for start_pos in xrange(len(string)):
+    for offset in xrange(len(string)):
         cur_dict = trie.root
         curlen = 1
         #check this substring
-        for pos, letter in enumerate(string[start_pos:]):
+        for pos, letter in enumerate(string[offset:]):
+            if letter not in cur_dict:
+                #back out of the trie and reset our word length tracker
+                cur_dict = trie.root
+                curlen = 1
             if letter in cur_dict:
-                #delve in to the trie
+                #delve into the trie
                 cur_dict = cur_dict[letter]
                 if _end in cur_dict:
                     #valid word
-                    word_ranges.add((start_pos + pos+1-curlen, start_pos + pos+1))
+                    word_ranges.add((offset + pos+1-curlen, offset + pos+1))
                 curlen += 1
-            else:
-                cur_dict = trie.root
-                curlen = 1
-                if letter in cur_dict:
-                    #delve in to the trie
-                    cur_dict = cur_dict[letter]
-                    if _end in cur_dict:
-                        #valid word
-                        word_ranges.add((pos-curlen, pos+1))
-                    curlen += 1
 
+    print word_ranges
     valid_words = []
     for word_range in word_ranges:
         valid_words.append(string[slice(*word_range)])
 
     return valid_words
 
-print triemethod('hotelfate', ['hotel', 'elf', 'fate', 'ate', 'hot'])
+print find_words('hotelfate', ['hotel', 'elf', 'fate', 'ate', 'hot'])
 
+
+import re
+
+
+def regexmethod(string, words):
+    valid_words = set()
+    regex = ""
+    for word in words:
+        regex += "%s|" % word
+        found = re.findall(regex, string)
+        for found_word in found:
+            valid_words.add(found_word)
+    return valid_words
+
+print regexmethod('hotelfate', ['hotel', 'elf', 'fate', 'ate', 'hot'])
